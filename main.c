b714dbf395e631bca45b0a09afcc5a73233ac7a2
aegiuss <rotten.swamp@mail.ru>
2020-09-02 22:24:11 +0300

    ...

--- main.c
+++ main.c
@@ -5,9 +5,15 @@ typedef struct Point {
 	double x, y;
 } Point;
 
+typedef struct Line {
+	double A, B, C; // Ax+By+C=0
+} Line;
+
 int main() {
-	Point A = {0}, B = {0}, C = {0}; // BC is a line segment
-	scanf("%d %d %d %d %d %d", &A.x, &A.y, &B.x, &B.y, &C.x, &C.y);
-	
-	printf("Line is %fX + %fY + %f\n", C.y-B.y, B.x-C.x, B.y*C.x-B.x*C.y);
+	Point A = {0}, B = {0}, C = {0}, K = {0}; // BC is a line segment, K is a point of intersect. 
+	scanf("%lf %lf %lf %lf %lf %lf", &A.x, &A.y, &B.x, &B.y, &C.x, &C.y);
+	Line BC = {C.y-B.y, B.x-C.x, B.y*C.x-B.x*C.y};
+	Line AK = {B.x-C.x, -C.y+B.y, (-B.x+C.x)*A.x + (C.y-B.y)*A.y};
+	printf("Line BC is %fx + %fy + %f\n", BC.A, BC.B, BC.C);
+	printf("Line AK is %fx + %fy + %f\n", AK.A, AK.B, AK.C);
 }
